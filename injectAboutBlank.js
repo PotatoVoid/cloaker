2a39510df44969261a970a7dcd68d8d1a5c2d558
PotatoVoid <sinclairs251@gmail.com>
2026-04-22 17:33:59 +0100

    First commit

---
+++ injectAboutBlank.js
@@ -0,0 +1,12 @@
+function inject(url) {
+  var tab = window.open();
+  tab.document.body.style.margin = "0";
+  tab.document.body.style.height = "100vh";
+  var iframe = tab.document.createElement("iframe");
+  iframe.style.border = "none";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.margin = "0";
+  iframe.src = url;
+  tab.document.body.appendChild(iframe);
+}
