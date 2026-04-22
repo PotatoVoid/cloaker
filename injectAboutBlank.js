ff82e5e5a68f1d6f3fbd0a78b51bda75ed9de6e0
PotatoVoid <sinclairs251@gmail.com>
2026-04-22 18:52:01 +0100

    Update injectAboutBlank.js
    Remove delay

--- injectAboutBlank.js
+++ injectAboutBlank.js
@@ -2,15 +2,13 @@ function inject(url) {
   var tab = window.open('about:blank', '_blank');
   tab.document.open();
   tab.document.write("<body><p>loading...</p></body>");
-  setTimeout(function() {
-    tab.document.body.style.margin = "0";
-    tab.document.body.style.height = "100vh";
-    var iframe = tab.document.createElement("iframe");
-    iframe.style.border = "none";
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
-    iframe.style.margin = "0";
-    iframe.src = url;
-    tab.document.write(iframe.outerHTML);
-  }, 2000);
+  tab.document.body.style.margin = "0";
+  tab.document.body.style.height = "100vh";
+  var iframe = tab.document.createElement("iframe");
+  iframe.style.border = "none";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.margin = "0";
+  iframe.src = url;
+  tab.document.write(iframe.outerHTML);
 }
