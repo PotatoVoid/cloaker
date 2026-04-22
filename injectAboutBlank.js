e540fce0e0b07e776ca81bf00168f2f1fe6e066d
PotatoVoid <sinclairs251@gmail.com>
2026-04-22 18:07:17 +0100

    Update injectAboutBlank.js
    Change to overwriting its HTML

--- injectAboutBlank.js
+++ injectAboutBlank.js
@@ -1,12 +1,7 @@
 function inject(url) {
-  var tab = window.open();
-  tab.document.body.style.margin = "0";
-  tab.document.body.style.height = "100vh";
-  var iframe = tab.document.createElement("iframe");
-  iframe.style.border = "none";
-  iframe.style.width = "100%";
-  iframe.style.height = "100%";
-  iframe.style.margin = "0";
-  iframe.src = url;
-  tab.document.body.appendChild(iframe);
+  document.documentElement.innerHTML = `
+  <head><title>Mail</title>
+  <style>iframe { width: 100%; height: 100% }</style></head>
+  <body><iframe src=[INSERT]></iframe></body>
+  `
 }
