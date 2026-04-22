7a9cb479aeec22e4832b9894b3a58371c62c9b04
PotatoVoid <sinclairs251@gmail.com>
2026-04-22 18:20:37 +0100

    Update injectAboutBlank.js
    Swapped document.documentElement.innerHTML = with document.write()

--- injectAboutBlank.js
+++ injectAboutBlank.js
@@ -1,7 +1,7 @@
 function inject(url) {
-  document.documentElement.innerHTML = `
+  document.write(`
   <head><title>Mail</title>
   <style>iframe { width: 100%; height: 100% }</style></head>
   <body><iframe src=[INSERT]></iframe></body>
-  `
+  `);
 }
